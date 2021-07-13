3238560653c7966d4ebbf4aedf54c344d8e0159b
Henry Fox <fengh16@163.com>
2021-07-13 10:19:10 +0800

    Update admin.py

--- moviegeeks/admin.py
+++ moviegeeks/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from moviegeeks.models import Movie
+from collector.models import Log
+from analytics.models import Rating, Cluster
+from recommender.models import MovieDescriptions, Similarity, Recs
 # Register your models here.
+
+admin.site.register(Movie)
+admin.site.register(Log)
+admin.site.register(Rating)
+admin.site.register(Cluster)
+admin.site.register(MovieDescriptions)
+admin.site.register(Similarity)
+admin.site.register(Recs)
